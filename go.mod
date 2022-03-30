77e9ceb466e3f5a687844c046822a8f28ae9ab20
Saketh Reddy <rsakethr1305@gmail.com>
2022-03-30 19:13:32 +0530

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/saketh-exp/grpc-go
+
+go 1.17
+
+require google.golang.org/grpc v1.45.0
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
---
+++ server/machine.go
@@ -0,0 +1,16 @@
+package server
+
+import (
+	"context"
+
+	"github.com/toransahu/grpc-eg-go/machine"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type MachineServer struct{}
+
+// Execute runs the set of instructions given.
+func (s *MachineServer) Execute(ctx context.Context, instructions *machine.InstructionSet) (*machine.Result, error) {
+	return nil, status.Error(codes.Unimplemented, "Execute() not implemented yet")
+}
